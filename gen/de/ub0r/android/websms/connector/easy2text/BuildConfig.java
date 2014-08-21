80a31b26b7f661caa834a536a83910b4ad5393aa
gminds <eddy2cold@yahoo.com>
2014-08-21 02:14:08 +0100

    Initial commit message

---
+++ gen/de/ub0r/android/websms/connector/easy2text/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.ub0r.android.websms.connector.easy2text;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
